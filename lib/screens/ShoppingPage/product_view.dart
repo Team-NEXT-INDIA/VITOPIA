a13ef989c1901bdb3d4c465c40a2fd1cbfc51afc
SatyanandAtluri <satyanand7789@gmail.com>
2023-01-24 19:22:19 +0530

    Modified product view page

--- lib/screens/ShoppingPage/product_view.dart
+++ lib/screens/ShoppingPage/product_view.dart
@@ -1,10 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProductDetailsView extends StatelessWidget {
-  const ProductDetailsView({Key? key}) : super(key: key);
-
+  ProductDetailsView({Key? key}) : super(key: key);
+  List<String> images = [
+    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png",
+    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png",
+    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png",
+    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png"
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.black,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios_new,
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+          title: const Text(
+            "DETAILS",
+            style: TextStyle(
+              fontFamily: 'Monument Extended',
+              color: Color(0xffFFFFFF),
+              fontSize: 29,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(bottom: 20),
+              width: double.infinity,
+              height: 300.h,
+              child: Image.network('https://i.ibb.co/GHFXdq9/t-shirt.jpg'),
+            ),
+            Expanded(
+                child: Stack(
+              children: [
+                Container(
+                  height: 900.h,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25),
+                      )),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Container(
+                              width: 60,
+                              height: 5,
+                              decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10.h),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15, top: 10),
+                        child: Text(
+                          'GRAPHIC GORILLA DESIGN',
+                          style: TextStyle(
+                            fontFamily: 'MMonteserrat',
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: Text(
+                          'T-shirts',
+                          style: TextStyle(
+                            fontFamily: 'Monteserrat',
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, left: 15),
+                        child: Container(
+                          height: 40.h,
+                          width: 100.h,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Buy Now',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ))
+          ],
+        ));
   }
 }
