39c63d58aa667a31c245a8ca77116223b5960b41
sam7075938131 <sam7075938131@gmail.com>
2023-02-05 00:43:16 +0530

    Added PDF Generated Invoice

--- lib/screens/ShoppingPage/InvoicePage.dart
+++ lib/screens/ShoppingPage/InvoicePage.dart
@@ -1,13 +1,82 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:path_provider/path_provider.dart';
 
-class InvoicePage extends StatelessWidget {
+import '../../customs/pdfviewer/full_pdf_viewer_scaffold.dart';
+
+class InvoicePage extends StatefulWidget {
   final invoice;
   final product;
   InvoicePage({Key? key, required this.invoice, required this.product})
       : super(key: key);
+
+  @override
+  State<InvoicePage> createState() => _InvoicePageState();
+}
+
+class _InvoicePageState extends State<InvoicePage> {
+  String? generatedPdfFilePath;
+
+  @override
+  void initState() {
+    super.initState();
+    generateExampleDocument();
+  }
+
+  Future<void> generateExampleDocument() async {
+    final htmlContent = """
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <style>
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        th, td, p {
+          padding: 5px;
+          text-align: left;
+        }
+        </style>
+      </head>
+      <body>
+        <h2>PDF Generated with flutter_html_to_pdf plugin</h2>
+        
+        <table style="width:100%">
+          <caption>Sample HTML Table</caption>
+          <tr>
+            <th>Month</th>
+            <th>Savings</th>
+          </tr>
+          <tr>
+            <td>January</td>
+            <td> ${widget.invoice['TXNAMOUNT']}</td>
+          </tr>
+          <tr>
+            <td>February</td>
+            <td>50</td>
+          </tr>
+        </table>
+        
+        <p>Image loaded from web</p>
+        <img src="https://i.imgur.com/wxaJsXF.png" alt="web-img">
+      </body>
+    </html>
+    """;
+
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    final targetPath = appDocDir.path;
+    final targetFileName = "example-pdf";
+
+    final generatedPdfFile = await FlutterHtmlToPdf.convertFromHtmlContent(
+        htmlContent, targetPath, targetFileName);
+    generatedPdfFilePath = generatedPdfFile.path;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +131,7 @@ class InvoicePage extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        invoice['TXNAMOUNT'],
+                        widget.invoice['TXNAMOUNT'],
                         style: GoogleFonts.montserrat(
                           fontSize: 17.sp,
                           fontWeight: FontWeight.w500,
@@ -81,7 +150,7 @@ class InvoicePage extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        invoice['TXNDATE'],
+                        widget.invoice['TXNDATE'],
                         style: GoogleFonts.montserrat(
                           fontSize: 17.sp,
                           fontWeight: FontWeight.w500,
@@ -235,7 +304,7 @@ class InvoicePage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
-                          product['name'],
+                          widget.product['name'],
                           style: GoogleFonts.montserrat(
                             color: Color(0xffBCBCBC),
                             fontWeight: FontWeight.w300,
@@ -243,7 +312,7 @@ class InvoicePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          product['price'],
+                          widget.product['price'],
                           style: GoogleFonts.montserrat(
                             fontSize: 15.sp,
                             fontWeight: FontWeight.w500,
@@ -253,7 +322,7 @@ class InvoicePage extends StatelessWidget {
                       ],
                     ),
                     Text(
-                      product['SKU'],
+                      widget.product['SKU'],
                       style: GoogleFonts.montserrat(
                         color: Color(0xffBCBCBC),
                         fontWeight: FontWeight.w300,
@@ -263,7 +332,19 @@ class InvoicePage extends StatelessWidget {
                   ],
                 ),
               ),
-            )
+            ),
+            ElevatedButton(
+              child: Text('Generate PDF'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => PDFViewerScaffold(
+                          appBar: AppBar(title: Text("Generated PDF Document")),
+                          path: generatedPdfFilePath ?? "")),
+                );
+              },
+            ),
           ],
         ),
       ),
